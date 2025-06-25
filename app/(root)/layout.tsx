298ed158820297274682d5ace1d50abd5fc28ea4
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-06-25 14:41:08 +0100

    i worked on the authorization using nextauth.js for github as a provider

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+export default function layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      <main className='font-work-sans'>{children}</main>
+    </>
+  );
+}
